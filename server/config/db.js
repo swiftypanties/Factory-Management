3404c3c0506316702e5a48e05e567d63152a48cb
Aviv David Malka <sonyon.winx@gmail.com>
2025-11-08 20:21:36 +0200

    config recongifured

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
+const connectDB = () => {
     mongoose
         .connect('mongodb://127.0.0.1:27017/Factory Management')
         .then(() => console.log("Connected to DB"))
